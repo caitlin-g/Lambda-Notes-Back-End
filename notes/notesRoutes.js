facc0aea1d1a80104f2f85228b89d96262a15829
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-29 16:19:46 -0400

    GET a list of notes endpoint is complete

--- notes/notesRoutes.js
+++ notes/notesRoutes.js
@@ -8,3 +8,15 @@ const jwt = require("jsonwebtoken");
 const knexConfig = require("../knexfile.js");
 
 const db = knex(knexConfig.development);
+
+//Get a list of all notes
+router.get("/notes", (req, res) => {
+  db("notes")
+    .select("id", "title", "note_text")
+    .then(notes => {
+      res.json(notes);
+    })
+    .catch(err => res.send(err));
+});
+
+module.exports = router;
